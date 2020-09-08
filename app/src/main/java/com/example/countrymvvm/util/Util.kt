fd101ffc850e8502d99a379753254c42cfc37c61
Dell <vaishjadhav35@gmail.com>
2020-09-08 16:59:05 +0800

    created mvvm structure and used disposable function of Rxjava

---
+++ app/src/main/java/com/example/countrymvvm/util/Util.kt
@@ -0,0 +1,24 @@
+package com.example.countrymvvm.util
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.countrymvvm.R
+
+fun getProgressDrawable(context: Context):CircularProgressDrawable{
+    return CircularProgressDrawable(context).apply {
+        strokeWidth=10f
+        centerRadius=30f
+
+        start()
+    }
+}
+ fun ImageView.loadImage(uri:String?,progressDrawable: CircularProgressDrawable){
+     val options=RequestOptions().placeholder(progressDrawable).error(R.mipmap.ic_launcher_round)
+     Glide.with(this.context)
+         .setDefaultRequestOptions(options)
+         .load(uri)
+         .into(this)
+ }
--- app/src/main/java/com/example/countrymvvm/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/countrymvvm/viewmodel/ListViewModel.kt
@@ -2,9 +2,16 @@ package com.example.countrymvvm.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.countrymvvm.model.CountriesService
 import com.example.countrymvvm.model.Country
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class ListViewModel : ViewModel() {
+    private val countriesService= CountriesService()
+    private  val disposable=CompositeDisposable()
     val countries = MutableLiveData<List<Country>>()
     val countryLoadError=MutableLiveData<Boolean>()
     val loading= MutableLiveData<Boolean>()
@@ -14,20 +21,29 @@ class ListViewModel : ViewModel() {
     }
 
     private fun fetchCountries() {
-        val mockData= listOf(Country("CountryA"),
-            Country("CountryB"),
-            Country("CountryC"),
-            Country("CountryD"),
-            Country("CountryE"),
-            Country("CountryF"),
-            Country("CountryG"),
-            Country("CountryH"),
-            Country("CountryI")
+        loading.value = true
+        disposable.add(
+            countriesService.getCountries()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object: DisposableSingleObserver<List<Country>>() {
 
-            )
+                    override fun onSuccess(value: List<Country>) {
+                        countries.value = value
+                        countryLoadError.value = false
+                        loading.value = false
+                    }
 
-        countryLoadError.value=false
-        loading.value=false
-        countries.value=mockData
+                    override fun onError(e: Throwable) {
+                        countryLoadError.value = true
+                        loading.value = false                    }
+
+                })
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
     }
 }
